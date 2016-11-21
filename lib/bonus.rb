238269772a3026d96fae91afc1c648be63a42ef2
Jenny Lai <jlai902@gmail.com>
2016-11-21 22:33:24 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -8,22 +8,26 @@ def bonus
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Steven", age: "30", attitude: "confused"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+          {name: "Mercutio", age: "18", attitude: "hot-headed"},
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
   }
 
-  #code your solution here:
+epic_tragedy[:montague][:hero][:name][:age][status] = "dead"
+epic_tragedy[:capulet][:heroine][:name][:age][status] = "dead"
 
-  
-
+# In lib/bonus.rb you'll see our completed epic_tragedy hash. We're coming to the end of the epic tragedy of Romeo and Juliet.
+# At this point in the story, Romeo and Juliet are—as in every good tragedy—quite dead. Use the above method to change the
+# status of our hero Romeo and our heroine Juliet from "alive" to "dead". These are bonus and if you're feeling comfortable
+# with Hashes, feel free to move forward. Also, to enable these tests make sure to remove the x in front of the it block in
+# spec/bonus_spec.rb.
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
